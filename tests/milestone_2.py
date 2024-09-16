cd9997aaf574f6e929e76cdb3a12f8db344815f4
Chaz <105102881+CHAZTANNO@users.noreply.github.com>
2024-09-16 21:31:20 +1000

    added test order data

---
+++ tests/milestone_2.py
@@ -0,0 +1,6 @@
+class Milestone2:
+    def __init__(self):
+        self.order_data = {'shelf': 1, 'bay': 2, 'height': 2, 'name': 'Ball'}
+    
+    def get_order(self):
+        return self.order_data #data as a dictionary
