897221036ba07622bc8743f5af51c555555d0c16
Chazka Tan <chaztan13@outlook.com>
2024-10-06 14:06:58 +1000

    mob led pin update

--- mobility/led.py
+++ mobility/led.py
@@ -1,22 +1,19 @@
-import RPi.GPIO as GPIO			# Import the GPIO module
+import RPi.GPIO as GPIO  # Import the GPIO module
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)		# Set the GPIO pin numbering scheme to BOARD
-    GPIO.setup(21, GPIO.OUT)		# Set pin 11 as an output pin
-    GPIO.setup(20, GPIO.OUT)		# Set pin 12 as an output pin
-    GPIO.setup(16, GPIO.OUT)		# Set pin 13 as an output pin
-
+    GPIO.setmode(GPIO.BOARD)         # Set the GPIO pin numbering scheme to BOARD
+    GPIO.setwarnings(False)          # Disable warnings
+    GPIO.setup(40, GPIO.OUT)         # Physical pin 40 corresponds to GPIO 21
+    GPIO.setup(38, GPIO.OUT)         # Physical pin 38 corresponds to GPIO 20
+    GPIO.setup(36, GPIO.OUT)         # Physical pin 36 corresponds to GPIO 16
 
 def set_LED(LED):
     led_pins = {
-        'RED': 21,
-        'YELLOW': 20,
-        'GREEN': 16
+        'RED': 40,                   # Physical pin 40
+        'YELLOW': 38,                # Physical pin 38
+        'GREEN': 36                  # Physical pin 36
     }
     
-    # if LED == currentLED:
-    #     return
-
     if LED == 'RED':
         GPIO.output(led_pins['RED'], GPIO.HIGH)
         GPIO.output(led_pins['YELLOW'], GPIO.LOW)
@@ -29,4 +26,3 @@ def set_LED(LED):
         GPIO.output(led_pins['RED'], GPIO.LOW)
         GPIO.output(led_pins['YELLOW'], GPIO.LOW)
         GPIO.output(led_pins['GREEN'], GPIO.HIGH)
-    
