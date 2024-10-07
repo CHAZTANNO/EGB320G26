30191f04e0ed7c09f5f70aa6fc395fe33ea8a7ab
Matthew Cugliari <mcugliar101@outlook.com>
2024-10-08 09:38:09 +1000

    added led init

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         packerBotSim.StartSimulator()
 
         navSystem = nav.NavClass()
-        #led.setup()
+        led.setup()
         mob.setupMob()
 
         
