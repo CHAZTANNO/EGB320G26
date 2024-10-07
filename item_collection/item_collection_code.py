751aacae8b43e2ec11dd85c84f848eab3accff11
Matthew Cugliari <mcugliar101@outlook.com>
2024-10-07 13:15:51 +1000

    lower lift

--- item_collection/item_collection_code.py
+++ item_collection/item_collection_code.py
@@ -81,7 +81,7 @@ def bringliftdown():
 # Testing
 if __name__ == "__main__":
     try:
-        collect_item(2)
+        bringliftdown()
         
     finally:
         cleanup()  # Ensure everything is cleaned up when done
