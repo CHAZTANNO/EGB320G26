28ce607f9ee85a15e393aac11024ebcf09a176b0
Matthew Cugliari <mcugliar101@outlook.com>
2024-10-08 09:04:59 +1000

    Changed led.py to match item collection GPIO config.

--- mobility/led.py
+++ mobility/led.py
@@ -2,28 +2,21 @@ import RPi.GPIO as GPIO  # Import the GPIO module
 import time
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)         # Set the GPIO pin numbering scheme to BOARD
-    GPIO.setwarnings(False)          # Disable warnings
-    GPIO.setup(40, GPIO.OUT)         # Physical pin 40 corresponds to GPIO 21
-    GPIO.setup(38, GPIO.OUT)         # Physical pin 38 corresponds to GPIO 20
-    GPIO.setup(36, GPIO.OUT)         # Physical pin 36 corresponds to GPIO 16
+    GPIO.setmode(GPIO.BCM)         # Set the GPIO pin numbering scheme to BOARD
+    GPIO.setup(21, GPIO.OUT)         # GPIO 21 = Physical pin 40 = RED
+    GPIO.setup(20, GPIO.OUT)         # GPIO 20 = Physical pin 38 = GREEN
+    GPIO.setup(16, GPIO.OUT)         # GPIO 16 = Physical pin 36 = BLUE
 
 def set_LED(LED):
-    led_pins = {
-        'RED': 40,                   # Physical pin 40
-        'YELLOW': 38,                # Physical pin 38
-        'GREEN': 36                  # Physical pin 36
-    }
-    
     if LED == 'RED':
-        GPIO.output(led_pins['RED'], GPIO.HIGH)
-        GPIO.output(led_pins['YELLOW'], GPIO.LOW)
-        GPIO.output(led_pins['GREEN'], GPIO.LOW)
+        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(20, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
     elif LED == 'YELLOW':
-        GPIO.output(led_pins['RED'], GPIO.HIGH)
-        GPIO.output(led_pins['GREEN'], GPIO.HIGH)
-        GPIO.output(led_pins['YELLOW'], GPIO.LOW)
+        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(16, GPIO.LOW)
     elif LED == 'GREEN':
-        GPIO.output(led_pins['RED'], GPIO.LOW)
-        GPIO.output(led_pins['YELLOW'], GPIO.LOW)
-        GPIO.output(led_pins['GREEN'], GPIO.HIGH)
+        GPIO.output(21, GPIO.LOW)
+        GPIO.output(20, GPIO.LOW)
+        GPIO.output(16, GPIO.GREEN)
