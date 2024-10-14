b29fcb3b5080ed7e8d5ae6bdc6f4e94f14acdeba
Matthew Cugliari <mcugliar101@outlook.com>
2024-10-14 12:24:59 +1000

    Changed motor orientation

--- mobility/milestone1-2.py
+++ mobility/milestone1-2.py
@@ -81,7 +81,7 @@ def control_loop(stdscr):
             # Move backward
             elif key == ord('s'):
                 board.motor_movement([board.M1], board.CW, speed)   # Motor 1 Backward
-                board.motor_movement([board.M2], board.CW, speed)    # Motor 2 Backward
+                board.motor_movement([board.M2], board.CCW, speed)    # Motor 2 Backward
                 stdscr.addstr(1, 0, "Moving backward    ")
 
             # Turn left
@@ -92,8 +92,8 @@ def control_loop(stdscr):
 
             # Turn right
             elif key == ord('d'):
-                board.motor_movement([board.M1], board.CW, speed)   # Motor 1 Backward
-                board.motor_movement([board.M2], board.CCW, speed)   # Motor 2 Forward
+                board.motor_movement([board.M1], board.CCW, speed)   # Motor 1 Backward
+                board.motor_movement([board.M2], board.CW, speed)   # Motor 2 Forward
                 stdscr.addstr(1, 0, "Turning right      ")
 
             # Stop the motors when no movement key is pressed
