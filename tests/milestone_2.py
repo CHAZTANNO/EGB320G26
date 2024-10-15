8163977490d337a09ce8bde0afb5bbe9bb585481
Chazka Tan <chaztan13@outlook.com>
2024-10-15 11:30:17 +1000

    objective change

--- tests/milestone_2.py
+++ tests/milestone_2.py
@@ -1,6 +1,6 @@
 class Milestone2:
     def __init__(self):
-        self.order_data = {'shelf': 2, 'bay': 2, 'height': 2, 'name': 'Bowl'}
+        self.order_data = {'shelf': 0, 'bay': 2, 'height': 2, 'name': 'Bowl'}
     
     def get_order(self):
         return self.order_data #data as a dictionary
