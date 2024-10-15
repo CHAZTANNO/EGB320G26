13631f202e1fae9f07031dc8a01c6d5d0d8c8037
Chazka Tan <chaztan13@outlook.com>
2024-10-15 11:26:15 +1000

    adjusted acceptance values

--- navigation/state_machine.py
+++ navigation/state_machine.py
@@ -52,7 +52,7 @@ class explorationState(State):
             if navSys.objectiveRow==0:
                 # row estimation identified
                 event = 'row_pose_estimated'
-            elif navSys.objectiveRow==1 and rowEstimate[0][0]<0.65:
+            elif navSys.objectiveRow==1 and rowEstimate[0][0]<0.50:
                 # row estimation identified
                 event = 'row_pose_estimated'
             elif navSys.objectiveRow==2 and rowEstimate[0][0]<0.45:
