8caa4578180dbf4a9bb3e3a765d5fd39e568f075
Chazka Tan <chaztan13@outlook.com>
2024-10-20 13:04:33 +1000

    tuning

--- navigation/state_machine.py
+++ navigation/state_machine.py
@@ -276,7 +276,7 @@ class approachItemState(State):
 
         print(seconds)
 
-        if seconds >= 2.5:
+        if seconds >= 2:
             event = 'item_close'
 
         if event=='item_close':
